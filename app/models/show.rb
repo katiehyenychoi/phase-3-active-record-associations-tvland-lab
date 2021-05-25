b69b98ae7b838a06244e75bf563ca6d7019323f1
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-05-25 16:49:00 -0400

    commit tree 7f2248231ad10cfb509acb5f74abc2e1be1e3200

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,22 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors , through: :characters
+  belongs_to :network
+
+  def actors_list
+  self.actors.map do |a|
+    a.full_name
+  end
+ end
+
 end
+
+# class Show < ActiveRecord::Base
+#   has_many :characters
+#   belongs_to :network
+#   has_many :actors, through: :characters
+
+#   def actors_list
+#     self.actors.map{|actor| actor.full_name}
+#   end
+# end
